c41df0746127bcd451c4b6fc509067ee882d8d4a
Avet <crykrypta@gmail.com>
2024-08-17 11:26:51 +0300

    test 1

--- config_data/config.py
+++ config_data/config.py
@@ -22,6 +22,9 @@ def load_config(path: str | None = None) -> Config:
     env = Env()
     env.read_env(path=path)
 
+    print("BOT_TOKEN:", env('BOT_TOKEN', 'Не найден'))
+    print("OPENAI_API_KEY:", env('OPENAI_API_KEY', 'Не найден'))
+    
     return Config(
         tg_bot=TgBot(token=env('BOT_TOKEN')),
         openai=OpenAI(token=env('OPENAI_API_KEY'))
