da02160223851270ea81dc2465623920e646cd9e
Avet <crykrypta@gmail.com>
2024-08-18 21:25:55 +0300

    comments on models

--- db/models.py
+++ db/models.py
@@ -3,11 +3,13 @@ from sqlalchemy.orm import DeclarativeBase, mapped_column, Mapped
 from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
 
 
+# Инициализация движка
 engine = create_async_engine(
     url='sqlite+aiosqlite://db.sqlite3',
     echo=True
 )
 
+# Сессия для подключения
 async_session = async_sessionmaker(engine)
 
 
@@ -15,13 +17,15 @@ class Base(DeclarativeBase):
     pass
 
 
+# Сущность пользователя
 class User(Base):
     __tablename__ = 'users'
     id: Mapped[int] = mapped_column(primary_key=True)
     tg_id = mapped_column(BigInteger, unique=True)
-    name: Mapped[str] = mapped_column(String(30))
+    name: Mapped[str] = mapped_column(String(32))
 
 
+# Сущность документа
 class Doc(Base):
     __tablename__ = 'docs'
     id: Mapped[int] = mapped_column(primary_key=True)
