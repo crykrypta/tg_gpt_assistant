6bfd1527afaab5e45e15d37f83ee73e36ce57710
Avet <crykrypta@gmail.com>
2024-08-19 17:22:29 +0300

    added new config module: db.pg_url

--- config_data/config.py
+++ config_data/config.py
@@ -13,9 +13,15 @@ class OpenAI:
 
 
 @dataclass
+class Database:
+    pg_url: str
+
+
+@dataclass
 class Config:
     tg_bot: TgBot
     openai: OpenAI
+    db: Database
 
 
 def load_config(path: str | None = None) -> Config:
@@ -24,7 +30,7 @@ def load_config(path: str | None = None) -> Config:
 
     print("BOT_TOKEN:", env('BOT_TOKEN', 'Не найден'))
     print("OPENAI_API_KEY:", env('OPENAI_API_KEY', 'Не найден'))
-    
+
     return Config(
         tg_bot=TgBot(token=env('BOT_TOKEN')),
         openai=OpenAI(token=env('OPENAI_API_KEY'))
